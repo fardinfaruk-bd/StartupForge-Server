31394c0796475639c11ba0a50e99c4fa747fbb34
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-06-20 19:20:33 +0600

    added patch method for update opportunity

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-const port = 5000
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-const uri = process.env.MONGO_DB_URI
+const uri = process.env.MONGO_DB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -34,14 +30,16 @@ async function run() {
 
     const database = client.db(process.env.DB_NAME);
     const opportunitiesCollection = database.collection("opportunities");
+    const startupCollection = database.collection("startup");
 
+    //opportunities related Api
     app.get("/api/opportunities", async (req, res) => {
       const query = {};
-      if(req.query.companyId){
-        query.companyId = req.query.companyId
+      if (req.query.startupId) {
+        query.startupId = req.query.startupId;
       }
-      if(req.query.status){
-        query.status = req.query.status
+      if (req.query.status) {
+        query.status = req.query.status;
       }
       const result = await opportunitiesCollection.find(query).toArray();
       res.send(result);
@@ -52,17 +50,50 @@ async function run() {
       const result = await opportunitiesCollection.findOne(query);
       res.send(result);
     });
-    
+
     app.post("/api/opportunities", async (req, res) => {
       const opportunity = req.body;
       const result = await opportunitiesCollection.insertOne(opportunity);
       res.send(result);
     });
-    
+
+    app.patch("/api/opportunities/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedOpportunity = req.body;
+
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: updatedOpportunity,
+      };
+      const result = await opportunitiesCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    //startup related Api
+    app.get("/api/my/startup", async (req, res) => {
+      const query = {};
+      if (req.query.founderId) {
+        query.founderId = req.query.founderId;
+      }
+      const result = await startupCollection.findOne(query);
+
+      res.send(result || {});
+    });
+    app.post("/api/startup", async (req, res) => {
+      const company = req.body;
+      const newCompany = {
+        ...company,
+        createdAt: new Date(),
+      };
+      const result = await startupCollection.insertOne(newCompany);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -70,7 +101,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
