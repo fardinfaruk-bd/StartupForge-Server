f8059978b414fa9879306da6c29c9694466cb699
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-06-21 23:14:56 +0600

    added get method to search opportunity by queryString using $regex, $or

--- index.js
+++ index.js
@@ -32,24 +32,67 @@ async function run() {
     const opportunitiesCollection = database.collection("opportunities");
     const startupCollection = database.collection("startup");
     const userCollection = database.collection("user");
+    const applicationCollection = database.collection("application");
 
     app.get("/api/users", async (req, res) => {
       const result = await userCollection.find().skip(2).toArray();
       res.send(result);
-    })
+    });
 
     //opportunities related Api
     app.get("/api/opportunities", async (req, res) => {
+      console.log("server side q", req.query);
       const query = {};
+      if (req.query.search) {
+        query.$or = [
+          { roleTitle: { $regex: req.query.search, $options: "i" } },
+          { startupName: { $regex: req.query.search, $options: "i" } },
+        ];
+      }
+
+      if (req.query.workType) {
+        query.workType = req.query.workType;
+      }
+      if (req.query.commitment) {
+        query.commitment = req.query.commitment;
+      }
+
+      // company related query
       if (req.query.startupId) {
         query.startupId = req.query.startupId;
       }
       if (req.query.status) {
         query.status = req.query.status;
       }
-      const result = await opportunitiesCollection.find(query).toArray();
+
+      
+      // pagination related work
+      if (req.query.page) {
+        const page = req.query.page;
+        const perPage = req.query.perPage || 12;
+        const skipItems = (page - 1) * perPage;
+
+        const total = await opportunitiesCollection.countDocuments(query);
+        const cursor = opportunitiesCollection
+          .find(query)
+          .skip(skipItems)
+          .limit(perPage);
+        const opportunities = await cursor.toArray();
+        return res.send({ total, opportunities });
+      }
+
+      const cursor = opportunitiesCollection.find(query);
+      const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/api/open/opportunities", async (req, res) => {
+        const query = { status: "active" };
+        const cursor = opportunitiesCollection.find(query).limit(3).sort({ createdAt: -1 });
+        const result = await cursor.toArray();
+        res.send(result);
+      })
+
     app.get("/api/opportunities/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -86,7 +129,7 @@ async function run() {
     app.get("/api/startups", async (req, res) => {
       const result = await startupCollection.find().skip(1).toArray();
       res.send(result);
-    })
+    });
     app.get("/api/my/startup", async (req, res) => {
       const query = {};
       if (req.query.founderId) {
@@ -105,6 +148,18 @@ async function run() {
       const result = await startupCollection.insertOne(newStartup);
       res.send(result);
     });
+
+
+    //application related Api
+    app.post("/api/applications", async (req, res) => {
+      const application = req.body;
+      const newApplication = {
+        ...application,
+        applied_at: new Date(),
+      }
+      const result = await applicationCollection.insertOne(newApplication);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
